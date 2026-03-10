38e2ab893ce0d031aca63ba7179b313a18a1feb0
sexyspy001 <mrshivamrajsharma@gmail.com>
2026-03-10 06:28:56 +0530

    Integrate Google Analytics in main.tsx
    Added Google Analytics tracking scripts to the main entry point.

--- CCH_CCH/src/main.tsx
+++ CCH_CCH/src/main.tsx
@@ -1,10 +1,27 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+// Google Analytics
+const GA_ID = "G-JN9GXS6VFF";
+
+const script1 = document.createElement("script");
+script1.async = true;
+script1.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
+document.head.appendChild(script1);
+
+const script2 = document.createElement("script");
+script2.innerHTML = `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${GA_ID}');
+`;
+document.head.appendChild(script2);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>
 );
